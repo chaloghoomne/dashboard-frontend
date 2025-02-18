535c6927e50cb7dec9c45746dca2a34bbed8823e
zairo984 <developer01zairo@gmail.com>
2025-02-18 12:48:12 +0530

    changed BASE_URL

--- src/Api/urls.js
+++ src/Api/urls.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://api.chaloghoomne.com/api/v1/";
-export const BASE_URL = "http://localhost:3001/api/v1/";
+export const BASE_URL = "https://dashboard-backend-r1xk.onrender.com/api/v1/";
+// export const BASE_URL = "http://localhost:3001/api/v1/";
 export const NetworkConfig = {
 	ADDCONTENT: "/add-content",
 	ADMINLOGIN: "admin-login",
