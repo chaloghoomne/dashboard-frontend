d3c44557762a31649f5c76abcd5797bddf53fac1
zairo984 <developer01zairo@gmail.com>
2025-02-18 13:50:39 +0530

    asd

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
-    chunkSizeWarningLimit: 4000, // Set higher limit (default is 500)
+    chunkSizeWarningLimit: 2000, // Set higher limit (default is 500)
     outDir: 'dist'
   },
 })
