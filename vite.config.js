b1e6e5c1d66045a65f060303999def8fae8c98bb
zairo984 <developer01zairo@gmail.com>
2025-02-18 13:54:09 +0530

    abc

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: {
-    chunkSizeWarningLimit: 2000, // Set higher limit (default is 500)
-    outDir: 'dist'
-  },
 })
