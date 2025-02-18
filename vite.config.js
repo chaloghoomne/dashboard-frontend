6da01b5f127361063aa04e27fa858f07e3b18f84
zairo984 <developer01zairo@gmail.com>
2025-02-18 13:29:43 +0530

    white page

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  base: '/',
   build: {
     chunkSizeWarningLimit: 1000, // Set higher limit (default is 500)
     outDir: 'dist'
