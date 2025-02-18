a72a0b97c888a429c9e7fff3ef551aad74c61739
zairo984 <developer01zairo@gmail.com>
2025-02-18 13:19:59 +0530

    white page problem

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Set higher limit (default is 500)
+    outDir: 'dist'
   },
 })
